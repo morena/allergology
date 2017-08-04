42e84238b58fb10968114fe77de843ac705d5e1a
Fiore, Morena <morena.fiore@mirumagency.com>
2017-08-04 17:36:56 +0100

    Started to work on catching the data

--- controllers/setup.js
+++ controllers/setup.js
@@ -1,18 +1,71 @@
 var express = require('express'),
     app = express();
 
+function LadderSetup(){
+    this.userId = null,
+    this.accessToken = null,
+    this.pausePrefs = null,
+    this.stagePrefs = null,
+    this.start = null,
+    this.shoppingDay = null,
+
+    this.setPausePrefs = function ( pause ){
+        this.pausePrefs = pause;
+    },
+    this.setStagePrefs = function ( stage ){
+        this.stagePrefs = stage;
+    },
+    this.setStart = function( start){
+        //convert start date to timestamp
+        this.start= start;
+    },
+    this.setShoppingDay = function( shopping ){
+        this.shoppingDay= shoppingDay;
+    },
+    this.getPausePrefs = function ( ){
+        return this.pausePrefs;
+    },
+    this.getStagePrefs = function ( ){
+        return this.stagePrefs;
+    },
+    this.getStart = function(){
+        return this.start;
+    },
+    this.getShoppingDay = function(){
+        //obtain timestamp of nearest date mathcing shoppingDay
+        return this.shoppingDay;
+    },
+    this.SaveUserInDb = function( userId ){
+    },
+    this.retrieveLadder = function( foodType ) {
+    },
+    this.planStages = function( stage ) {
+        //add starting date to calendar
+        //using the stage length unit and number, add 
+        //var numberOfDaysToAdd = this.stagePrefs.num;
+        //startDate.setDate(startDate.getDate() + numberOfDaysToAdd);
+    }
+}
     exports.init = function(req, res){
         if(!req.body){
             return res.sendStatus(400);
         }
-        console.log(req.body);
+        var ladderSetup = null;
+
+        //console.log(req);
+        //console.log(req.originalRequest.data.user.accessToken);
+        //console.log(req.body);
+        console.log(req.body.originalRequest.data.user);
+        if(req.body.originalRequest.length() > 0){
+            var userId = req.body.originalRequest.data.user.userId,
+                accessToken = req.body.originalRequest.data.user.accessToken;
+        }
+        //console.log(req);
 
         var data = req.body.result.parameters,
             allergyType = data.allergyType,
-            stageDurationNum = data.stageDuration.amount,
-            stageDurationType = data.stageDuration.unit,
-            pauseNum = data.pause.amount,
-            pauseType = data.pause.unit,
+            stage = data.stageDuration,
+            pause = data.pause,
             shopping = data.shopping,
             start = data.date;
 
@@ -21,4 +74,11 @@ var express = require('express'),
         console.log(stageDurationNum +' '+ stageDurationType);
         console.log(pauseNum +' '+pauseType);
         console.log(shopping);
+
+        ladderSetup = new LadderSetup();
+
+        ladderSetup.setPausePrefs(pause);
+        ladderSetup.setStagePrefs(stage);
+        ladderSetup.setstart(start);
+        ladderSetup.setShoppingDay(shopping);
     };
